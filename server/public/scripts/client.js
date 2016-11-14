f395195f8e6cfc000b9cbcf0c6eab2ac057b73d6
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-14 15:44:51 -0600

    finished project this time

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,47 @@
-console.log('js loaded');
+$(document).ready(function() {
+  console.log('up and running!');
+
+  getJokes();
+
+  function getJokes() {
+    $.ajax({
+      type: 'GET',
+      url: '/jokes',
+      success: function(data) {
+        console.log('got the joke data!');
+        appendJokes(data);
+      }
+    });
+  }
+
+  function appendJokes(jokes) {
+    $("#jokeBox").empty();
+
+    for (var i = 0; i < jokes.length; i++) {
+      $("#jokeBox").append('<div><h2>' + jokes[i].whoseJoke + '</h2><p>' + jokes[i].jokeQuestion + '</p><p>' + jokes[i].punchLine +'</p></div>');
+    }
+
+  }
+
+  $("#jokeForm").on("submit", postJoke);
+
+  function postJoke(newJoke) {
+    event.preventDefault();
+    var newJoke = {};
+
+    $.each($('#jokeForm').serializeArray(), function(i, field) {
+      newJoke[field.name] = field.value;
+    });
+
+    console.log(newJoke);
+
+    $.ajax({
+      type: 'POST',
+      url: '/jokes',
+      data: newJoke,
+      success: function(data) {
+        getJokes();
+      }
+    });
+  }
+});
